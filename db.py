67bbf6408ae4140b119b2b019e0f19fbd0779d90
Min Thant <minthantoo360@gamil.com>
2026-01-16 20:39:25 +0630

    establish connection to DB

---
+++ db.py
@@ -0,0 +1,14 @@
+import psycopg2
+db_name = "TaskListDB"
+db_user = "tasklist_user"
+db_pass = "abc123"
+db_host = "localhost"
+
+def getTaskList():
+    conn = psycopg2.connect(dbname=db_name,user = db_user,password = db_pass,host = db_host)
+    cur = conn.cursor()
+    cur.execute('SELECT task_name,is_active FROM public."TaskList"')
+    tasklist = cur.fetchall()
+    cur.close
+    conn.close
+    return tasklist
