07c1cd12f286f1862e7705a0830fe8524988a642
Min Thant <minthantoo360@gamil.com>
2026-01-16 22:34:51 +0630

    DB joined

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-import psycopg2
+import psycopg2 # to connect db,request and fetch
 db_name = "TaskListDB"
 db_user = "tasklist_user"
 db_pass = "abc123"
@@ -6,7 +6,7 @@ db_host = "localhost"
 
 def getTaskList():
     conn = psycopg2.connect(dbname=db_name,user = db_user,password = db_pass,host = db_host)
-    cur = conn.cursor()
+    cur = conn.cursor() # cursor is a bridge between db and python
     cur.execute('SELECT task_name,is_active FROM public."TaskList"')
     tasklist = cur.fetchall()
     cur.close
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from db import getTaskList
 app = Flask(__name__)
-tasklist = [["Walk Dog",True],["Wash Dishes",False]]
-#askList = getTaskList
+#tasklist = [["Walk Dog",True],["Wash Dishes",False]]
+tasklist = getTaskList()
 @app.route("/")
 def home():     
     return render_template("tasklist.html",TaskList = tasklist) # r_t -->to read html file and then to load data from python, to run on browser
